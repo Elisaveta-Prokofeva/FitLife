55d3b3853baa4ce200042a2086a1070e4759b66a
Елизавета Прокофьева <kaktusru8@gmail.com>
2026-08-11 09:43:48 +0300

    Правки внесены

--- fit_life.py
+++ fit_life.py
@@ -1,21 +1,28 @@
+WATER_PER_KG = 30 # вода на кг
+ML_TO_LITERS = 1000 # мл в литры
+
 while True:
     try:
         user_name = input('Добро пожаловать! Как вас зовут? ')
         user_age_input = input('Сколько вам полных лет? ')
         user_age = int(user_age_input)
 
-        user_weight_input = input('Подскажите, пожалуйста, ваш вес? (в кг) ')
+        user_weight_input = input(
+            'Подскажите, пожалуйста, ваш вес в кг? (Например, 60)
+        ) # добавила здесь уточнение. "кг" вынесла в вопрос, чтоб не путало в примере
         user_weight = float(user_weight_input)
 
-        user_height_input = input('Какой у вас рост? (в метрах) ')
+        user_height_input = input(
+            'Какой у вас рост в метрах (Например, 1.70)
+            ) # аналогично тому, что выше
         user_height = float(user_height_input)
 
         if user_height <= 0 or user_weight <= 0:
             raise ValueError('Рост и вес должны быть положительными числами.')
 
         bmi = round(user_weight / (user_height ** 2), 1)
-        water_ml = user_weight * 30
-        water_l = round(water_ml / 1000, 1)
+        water_ml = user_weight * WATER_PER_KG
+        water_l = round(water_ml / ML_TO_LITERS, 1)
 
         print(f'Отчет для {user_name}, ({user_age})')
         print(f'Ваш ИМТ {bmi}')
